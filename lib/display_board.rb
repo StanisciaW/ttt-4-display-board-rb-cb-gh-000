7411bbe7930b3ec819d7c3e55a37be9c0408e8b8
William Staniscia <william.staniscia@outlook.com>
2019-10-17 21:59:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,33 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  cell_x = "X"
+  cell_o = "O"
+  cell = "  "
+  separator = "|"
+  lines = "-----------"
+
+  puts "#{cell_x}#{separator}#{cell_x}#{separator}#{cell_x}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell_x}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+
+  puts "#{cell_o}#{separator}#{cell}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+
+  puts "#{cell_o}#{separator}#{cell}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell_x}#{separator}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+end
